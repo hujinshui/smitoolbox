94ff8a3718f9867ecc7e3a00da73b17b8976d1ff
Dahua Lin [on CSAIL Debian] <Dahua Lin [on CSAIL Debian]@localhost>
2010-05-26 20:39:42 -0400

    modify confusmat to support different numbers of classes for L1 and L2

--- calc/confusmat.m
+++ calc/confusmat.m
@@ -12,6 +12,13 @@ function C = confusmat(m, L1, L2, op)
 %       Then C is a matrix of size m x m, where C(k, l) is the number
 %       of samples with L1(i) == k and L2(i) == l.
 %
+%   C = confusmat([m1, m2], L1, L2);
+%       computes the confusion matrix between the labels in L1 and L2,
+%       where L1 and L2 are respectively based on m1 and m2 classes.
+%
+%       Actually, confusmat(m, L1, L2) is equivalent to 
+%       confusmat([m m], L1, L2).
+%
 %   C = confusmat(m, L1, L2, 'nrm')
 %       computes the normalized confusion matrix, of which the sum of
 %       all entries are normalized to one.
@@ -32,8 +39,16 @@ function C = confusmat(m, L1, L2, op)
 
 %% verify input arguments
 
-if ~(isnumeric(m) && isscalar(m) && m > 0)
-    error('confusmat:invalidarg', 'm should be a positive scalar.');
+if ~(isnumeric(m) && (isscalar(m) || numel(m) == 2))
+    error('confusmat:invalidarg', 'm should be a either a scalar or a pair.');   
+end
+
+if isscalar(m)
+    m1 = m;
+    m2 = m;
+else
+    m1 = m(1);
+    m2 = m(2);
 end
 
 if ~(isvector(L1) && isvector(L2) && isnumeric(L1) && isnumeric(L2) && ...
@@ -42,10 +57,10 @@ if ~(isvector(L1) && isvector(L2) && isnumeric(L1) && isnumeric(L2) && ...
         'L1 and L2 should be numeric vectors of the same size.');
 end
 
-if any(L1 > m) || any(L1 < 1)
+if any(L1 > m1) || any(L1 < 1)
     error('confusmat:invalidarg', 'Some labels in L1 exceed range.');
 end
-if any(L2 > m) || any(L2 < 1)
+if any(L2 > m2) || any(L2 < 1)
     error('confusmat:invalidarg', 'Some labels in L2 exceed range.');
 end
 
@@ -62,13 +77,13 @@ end
 
 %% main
 
-I = L1 + m * (L2 - 1);
-C = intcount([1, m * m], I);
+I = L1 + m1 * (L2 - 1);
+C = intcount([1, m1 * m2], I);
 
 if nrm
     C = C / sum(C);
 end
 
-C = reshape(C, m, m);
+C = reshape(C, m1, m2);
 
 
