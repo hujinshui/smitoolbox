d4dabdc517be3307dae92514391effcb780e389e
Dahua Lin [on Macbook Pro] <Dahua Lin [on Macbook Pro]@localhost>
2010-06-11 15:31:51 -0700

    add & modify small mat computation

--- matrix/test_mat2x2c.m
+++ matrix/test_mat2x2c.m
@@ -8,6 +8,9 @@ function test_mat2x2c(varargin)
 %   'inv':      matrix inverse
 %   'det':      matrix determinant
 %   'trace':    matrix trace
+%   'polarm':   polar representation
+%   'sqrtm':    matrix square root
+%   'chol':     Cholesky decomposition
 %
 
 % Created by Dahua Lin, on Apr 7, 2010
@@ -16,7 +19,7 @@ function test_mat2x2c(varargin)
 %% verify input arguments
 
 assert(iscellstr(varargin) && ... 
-    all(ismember(varargin, {'inv', 'det', 'trace'})), ...
+    all(ismember(varargin, {'inv', 'det', 'trace', 'polarm', 'sqrtm', 'chol'})), ...
     'test_mat2x2c:invalidarg', ...
     'Invalid item.');
 
@@ -157,8 +160,138 @@ r30 = X3(1,:) + X3(3,:);
 
 % compare result
 
-compare('det_gm', r10, r1, 1e-15);
-compare('det_sm', r30, r3, 1e-15);
+compare('trace_gm', r10, r1, 1e-15);
+compare('trace_sm', r30, r3, 1e-15);
+
+
+
+function test_polarm() %#ok<DEFNU>
+
+n = 1000;
+
+X0 = gen_pdm(n);
+
+% test 4 x n form
+
+X1 = X0([1 2 2 3], :);
+R1 = polarm2x2(X1);
+n1 = size(X1, 2);
+assert(isequal(size(R1), [3 n1]));
+
+% test 2 x 2 x n form
+
+X2 = reshape(X1, [2 2 n1]);
+R2 = polarm2x2(X2);
+assert(isequal(size(R2), [3 n1]));
+
+% test 3 x n form
+
+X3 = X0;
+R3 = polarm2x2(X3);
+assert(isequal(size(R3), [3 n1]));
+
+assert(isequal(R1, R2, R3));
+assert(all(R1(1,:) >= R1(2,:)));
+
+
+% generate ground-truth
+
+M0 = X2;
+M1 = zeros(size(M0));
+for i = 1 : n1
+    M1(:,:,i) = from_polar(R1(:,i));
+end
+
+% compare result
+
+compare('polarm_sm', M0, M1, 2e-14);
+
+
+
+
+function test_sqrtm() %#ok<DEFNU>
+
+n = 1000;
+
+X0 = gen_pdm(n);
+
+% test 4 x n form
+
+X1 = X0([1 2 2 3], :);
+R1 = sqrtm2x2(X1);
+n1 = size(X1, 2);
+assert(isequal(size(R1), [4 n1]));
+
+% test 2 x 2 x n form
+
+X2 = reshape(X1, [2 2 n1]);
+R2 = sqrtm2x2(X2);
+assert(isequal(size(R2), [2 2 n1]));
+
+% test 3 x n form
+
+X3 = X0;
+R3 = sqrtm2x2(X3);
+assert(isequal(size(R3), [3 n1]));
+
+R2a = reshape(R2, 4, n1);
+assert(isequal(R1, R2a, R3([1 2 2 3], :)));
+
+% generate ground-truth
+
+M0 = X2;
+M1 = zeros(size(M0));
+for i = 1 : n1
+    M1(:,:,i) = R2(:,:,i) * R2(:,:,i);
+end
+
+% compare result
+
+compare('sqrtm_sm', M0, M1, 2e-14);
+
+
+
+function test_chol() %#ok<DEFNU>
+
+n = 1000;
+
+X0 = gen_pdm(n);
+
+% test 4 x n form
+
+X1 = X0([1 2 2 3], :);
+R1 = chol2x2(X1);
+n1 = size(X1, 2);
+assert(isequal(size(R1), [4 n1]));
+assert(all(R1(3, :) == 0));
+
+% test 2 x 2 x n form
+
+X2 = reshape(X1, [2 2 n1]);
+R2 = chol2x2(X2);
+assert(isequal(size(R2), [2 2 n1]));
+
+% test 3 x n form
+
+X3 = X0;
+R3 = chol2x2(X3);
+assert(isequal(size(R3), [3 n1]));
+
+R2a = reshape(R2, 4, n1);
+R3a = [R3(1:2, :); zeros(1, n1); R3(3,:)];
+assert(isequal(R1, R2a, R3a));
+
+% generate ground-truth
+
+M0 = X2;
+M1 = zeros(size(M0));
+for i = 1 : n1
+    M1(:,:,i) = R2(:,:,i) * R2(:,:,i)';
+end
+
+% compare result
+
+compare('chol_sm', M0, M1, 2e-14);
 
 
 
@@ -193,6 +326,19 @@ dv = X(1,:) .* X(3,:) - X(2,:).^2;
 si = abs(dv) > 1e-3;
 
 X = X(:, si);
+
+
+function M = from_polar(p)
+
+a = p(1);
+b = p(2);
+t = p(3);
+
+c = cos(t);
+s = sin(t);
+
+R = [c -s; s c];
+M = R * [a 0; 0 b] * R';
 
 
 
