ca027ade19067b4ba2e89a169a83de35c3133424
Dahua Lin [on Macbook Pro] <Dahua Lin [on Macbook Pro]@localhost>
2010-07-22 00:53:00 -0700

    update nrmdot

--- metrics/nrmdot.m
+++ metrics/nrmdot.m
@@ -17,18 +17,17 @@ function v = nrmdot(X1, X2)
 
 %   History
 %       - Created by Dahua Lin, on Jun 2, 2008
+%       - Modified by Dahua Lin, on Jul 22, 2010
+%           - simplify error handling
 %
 
 %% parse and verify input arguments
 
-assert(isnumeric(X1) && isnumeric(X2) && ndims(X1) == 2 && ndims(X2) == 2, ...
-    'nrmdot:invalidarg', ...
-    'X1 and X2 should be both numeric matrices.');
+if ~(isfloat(X1) && isfloat(X2) && ndims(X1) == 2 && ndims(X2) == 2)
+    error('nrmdot:invalidarg', ...
+        'X1 and X2 should be both numeric matrices.');
+end
 
-[d, n] = size(X1);
-assert(size(X2, 1) == d && size(X2, 2) == n, ...
-    'nrmdot:invalidsize', ...
-    'X1 and X2 should be of the same size.');
 
 %% main
 
@@ -37,5 +36,3 @@ s2 = sum(X2 .* X2, 1);
 
 v = sum(X1 .* X2, 1) ./ sqrt(s1 .* s2);
 
-v(v > 1) = 1;
-v(v < -1) = -1;
