91b2b001fe84739424323fd7b1d91a78d78ad80c
Dahua Lin [on Macbook Pro] <Dahua Lin [on Macbook Pro]@localhost>
2010-08-01 23:37:14 -0700

    remove old norm funcs

--- metrics/chebydist.m
+++ metrics/chebydist.m
@@ -21,14 +21,11 @@ function dists = chebydist(X1, X2)
 
 %% parse and verify input arguments
 
-assert(isnumeric(X1) && isnumeric(X2) && ndims(X1) == 2 && ndims(X2) == 2, ...
-    'chebydist:invalidarg', ...
-    'X1 and X2 should be both numeric matrices.');
+if ~(isnumeric(X1) && isnumeric(X2) && ndims(X1) == 2 && ndims(X2) == 2)
+    error('chebydist:invalidarg', ...
+        'X1 and X2 should be both numeric matrices.');
+end
 
-[d, n] = size(X1);
-assert(size(X2, 1) == d && size(X2, 2) == n, ...
-    'chebydist:invalidsize', ...
-    'X1 and X2 should be of the same size.');
 
 %% main
 
