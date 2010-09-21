10880ff551a3322e8068535478ed499ef1799261
Dahua [Richards] <Dahua [Richards]@localhost>
2010-09-21 18:00:59 -0400

    mrf2d corrected

--- mrf/mrf1d.m
+++ mrf/mrf1d.m
@@ -1,4 +1,4 @@
-function W = mrf1d(n, kernel)
+function W = mrf1d(n, kernel, offsets)
 % Construct a second-order MRF between nodes with 1D index set
 %
 %   W = mrf1d(n, kernel);
@@ -11,8 +11,22 @@ function W = mrf1d(n, kernel)
 %       size n x n, where W(i, i+k) and W(i, i-k) are set to kernel(k),
 %       when k <= h, otherwise they are set to zeros
 %       
+%   W = mrf1d(n, kernel, offsets)
+%       returns the affinity matrix of the MRF constructed as follows:
+%
+%       Here, kernel specifies the weights between pairs of nodes with 
+%       specified offsets. Concretely, it will set W(i, i+offsets(k))
+%       and W(i, i-offsets(k)) to kernel(k).
+%
+%       Note that if the length of kernel is h, then mrf1d(n, kernel)
+%       is equivalent to mrf1d(n, kernel, 1:h);
+%
 
-% Created by Dahua Lin, on Apr 16, 2010
+%   History
+%   -------
+%       - Created by Dahua Lin, on Apr 16, 2010
+%       - Modified by Dahua Lin, on Sep 21, 2010
+%           - 
 %
 
 %% verify input arguments
@@ -24,32 +38,48 @@ end
 if ~(isfloat(kernel) && isreal(kernel) && isvector(kernel))
     error('mrf1d:invalidarg', 'kernel should be a real vector.');
 end
-
 h = numel(kernel);
-if n <= h
-    error('mrf1d:invalidarg', 'n should be greater than the kernel size.');
+
+if nargin < 3
+    offsets = 1 : h;
+else
+    if ~(isnumeric(offsets) && isequal(size(offsets), size(kernel)) && ...
+            all(offsets == fix(offsets)))
+        error('mrf1d:invalidarg', ...
+            'offsets should be an array of integers with same size of kernel.');
+    end
 end
+
 
 %% main
 
+kz = kernel == 0;
+if any(kz)
+    offsets = offsets(~kz);
+    kernel = kernel(~kz);
+    h = numel(kernel);
+end
+
 if h == 1
-    I = 1:n-1;
-    J = 2:n;
-    V = kernel(ones(1,n-1));
+    I = 1 : n-offsets;
+    J = 1+offsets : n;
+    V = kernel(ones(1, n-offsets));
         
 else
-    ns = n * h - h * (1+h) / 2;
+    ns = n * h - sum(offsets);
     I = zeros(1, ns);
     J = zeros(1, ns);
     V = zeros(1, ns);
     
     i = 0;
     for k = 1 : h
-        si = i+1 : i+(n-k);        
-        I(si) = 1 : n-k;
-        J(si) = 1+k : n;
+        o = offsets(k);
+        
+        si = i+1 : i+(n-o);        
+        I(si) = 1 : n-o;
+        J(si) = 1+o : n;
         V(si) = kernel(k);        
-        i = i + (n-k);
+        i = i + (n-o);
     end
 end
 
--- mrf/test_mrf2d.m
+++ mrf/test_mrf2d.m
@@ -1,32 +1,19 @@
-function W = test_mrf2d(m, n, kernel, roi)
+function W = test_mrf2d(siz, kernel)
 
-if nargin < 4
-    roi = empty;
-end
 
-use_roi = ~isempty(roi);
+m = siz(1);
+n = siz(2);
 
-N0 = m * n;
 h = size(kernel, 1) - 1;
 w = size(kernel, 2) - 1;
 
-if use_roi
-    N = nnz(roi);
-    imap = zeros(m, n);
-    imap(roi) = 1 : N;
-else
-    N = N0;
-end
+N = m * n;
+
 
 W = zeros(N, N);
-for i = 1 : N0
-    for j = 1 : N0
-        
-        if use_roi && ~(roi(i) && roi(j))
-            continue;
-        end
-        
-                        
+for i = 1 : N
+    for j = 1 : N
+                                
         [iy, ix] = ind2sub([m, n], i);
         [jy, jx] = ind2sub([m, n], j);
         
@@ -34,11 +21,7 @@ for i = 1 : N0
         dy = abs(jy - iy);
                         
         if dx <= w && dy <= h
-            if use_roi
-                W(imap(i), imap(j)) = kernel(dy+1, dx+1);
-            else
-                W(i, j) = kernel(dy+1, dx+1);
-            end
+            W(i, j) = kernel(dy+1, dx+1);
         end
         
     end
