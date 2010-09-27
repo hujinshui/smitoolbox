e26885cc718471995e7c0e18b6a4cf138a625349
Dahua Lin [on Macbook Pro] <Dahua Lin [on Macbook Pro]@localhost>
2010-09-27 01:49:55 -0400

    add kmeans_alg

---
+++ classics/cluster/kmeans_alg.m
@@ -0,0 +1,116 @@
+classdef kmeans_alg
+    % The class to implement the K-means algorithm
+    %
+    % This class implementes Standard K-means algorithm in two ways:
+    %
+    %   - Conventional way: it computes the distances between each pair
+    %     of sample and center at each iteration.
+    %
+    %   - Accelerated way: it avoids redundant distance computation 
+    %     using triangle inequality, as described by the following paper:
+    %
+    %       Charles Elkan. "Using the Triangle Inequality to Accelerate 
+    %       k-Means". Proceedings of 20th International Conference on
+    %       Machine Learning (ICML-03), Washington DC. 2003.
+    %
+    
+    %   History
+    %   -------
+    %       - Created by Dahua Lin, on Sep 27, 2010
+    %
+    
+    
+    properties
+        max_iter = 300;
+        
+        cost_func = @pwsqL2dist;
+        dist_func = @pwL2dist;
+        mean_func = @vecmean;        
+    end
+    
+    
+    methods                      
+        
+        function [L, M, info] = run_std(alg, X, M0)
+            % Runs K-means in the conventional way
+            %
+            
+            %% intialization
+            
+            cfunc = alg.cost_func;
+            mfunc = alg.mean_func;
+            
+            M = M0;
+            K = size(M, 2);
+            Gp = cell(1, K);
+            
+            cost = cfunc(M, X);
+            [mcv, L] = min(cost, [], 1);
+            G = intgroup([1, K], L);
+            
+            
+            %% iterations
+            
+            maxit = alg.max_iter;
+            it = 0;
+            converged = false;
+            
+            while ~converged && it < maxit 
+                
+                it = it + 1;
+                
+                % update centers                                
+                
+                M = kmeans_alg.update_means(M, X, Gp, G, mfunc);
+                                
+                % update labeling
+                
+                cost = cfunc(M, X);
+                Lp = L;
+                [mcv, L] = min(cost, [], 1);
+                
+                if ~isequal(Lp, L)
+                    Gp = G;
+                    G = intgroup([1, K], L);
+                else
+                    converged = true;
+                end                                
+            end
+            
+            %% output info
+            
+            if nargout >= 3
+                info.converged = converged;
+                info.niters = it;
+                info.totalcost = sum(mcv);
+            end
+
+        end               
+        
+        
+    end
+    
+    
+    methods(Static, Access='private')        
+        
+        function M = update_means(M, X, Gp, G, mfunc)
+            % update mean vector(s)
+            %
+            %   Gp: previous grouping
+            %   G:  current grouping
+            %
+            
+            K = size(M, 2);
+            
+            for k = 1 : K                
+                gk = G{k};
+                if ~isequal(Gp{k}, gk) && ~isempty(gk)                    
+                    M(:, k) = mfunc(X(:, gk));   
+                end                
+            end
+        end
+        
+    end
+    
+    
+end
