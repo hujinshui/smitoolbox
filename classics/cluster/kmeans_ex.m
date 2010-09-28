148967eb5e7f937e4bd05e736a08a9124a611461
Dahua Lin [on CSAIL Debian] <Dahua Lin [on CSAIL Debian]@localhost>
2010-09-28 02:04:06 -0400

    kmeans_ex tested

--- classics/cluster/kmeans_ex.m
+++ classics/cluster/kmeans_ex.m
@@ -28,8 +28,13 @@ function [L, M, info] = kmeans_ex(X, M0, varargin)
 %
 %       Options:
 %       
-%       The user can specify options (in name/value pairs or struct) to 
+%       The user can specify options (in name/value pairs) to 
 %       control the algorithm. The options are listed as below:
+%
+%       - scheme:   the scheme to use. (default = 'auto')
+%                   It can be any of the following values:
+%                   - 'std':   use standard scheme
+%                   - 'acc':   use accelerated scheme
 %
 %       - max_iter: the maximum number of iterations (default = 100)
 %
@@ -49,7 +54,7 @@ function [L, M, info] = kmeans_ex(X, M0, varargin)
 %                   of the following values (default = 'km++'):
 %                   - 'random':  randomly pick K distinct samples as centers
 %                   - 'km++':    randomly pick K samples using Kmean++
-%                   - 'mdinit':  randomly select the first one, and then
+%                   - 'mcinit':  randomly select the first one, and then
 %                                sequentially select furthest samples 
 %                                from selected centers.                   
 %
@@ -60,16 +65,27 @@ function [L, M, info] = kmeans_ex(X, M0, varargin)
 %                   - 'repick':     randomly repick a sample as new center
 %                   - 'repick++':   randomly repick a sample as new center
 %                                   using moderated scheme as in Kmeans++
-%                   - 'mdpick':     pick the sample that is farthest to
-%                                   all other centers
+%                   - 'mcpick':     pick the sample that is farthest to
+%                                   all other centers (with maximum cost)
 %                   - 'error':      raise an error.
-%                   - 'keep':       simply keep that center without change.  
+%                   - 'keep':       simply keep that center without change.
 %
-
-%   History
-%   -------
-%       - Created by Dahua Lin, on Sep 27, 2010
-%     
+%       - dist_func:  the distance function (or its name)
+%                     It can take either of the following values:
+%                     - 'sqL2':  use squared L2 distance as cost, and
+%                                component-wise arithmetic mean as center
+%                     - 'L1':    use L1 distance as cost, and 
+%                                component-wise median as center
+%                       
+%                     Or, it can be a user-defined function handle. 
+%                     (refer to the remarks below for details).
+%
+%       - rstream:  the random number stream to use, which can be either
+%                   empty (using default) or a RandStream object.
+%                   (default = []).
+%
+%       The user can also use kmeans_ex_set function to construct
+%       an option struct and input it to this function.   
 %
 %   Remarks
 %   -------
@@ -95,8 +111,10 @@ function [L, M, info] = kmeans_ex(X, M0, varargin)
 %         
 %         We note that while the accelerated way avoids unnecessary 
 %         computation, it would on the other hand incurs overhead of
-%         book-keeping and subset selection. Generally, we recommend 
-%         using the accelerated way when K is not very small.
+%         book-keeping and subset selection. Moreover, the accelerated
+%         scheme makes it difficult to vectorize some of the codes.
+%         Hence, the overall run-time efficiency depends on particular
+%         data set. By default, the function uses standard way.
 %
 %         Moreover, in our implementation, we avoid necessary
 %         re-computation of mean-vectors, by only computing those affected
@@ -129,10 +147,328 @@ function [L, M, info] = kmeans_ex(X, M0, varargin)
 %       - Created by Dahua Lin, on Sep 27, 2010
 %
     
+
+%% parse and verify input
+
+if ~(isfloat(X) && ndims(X) == 2)
+    error('kmeans_ex:invalidarg', 'X should be a numeric matrix.');
+end
+
+[d, n] = size(X);
+if isscalar(M0)
+    K = M0;
+    M0 = [];
+else
+    K = size(M0, 2);
+    if ~(isfloat(M0) && ndims(M0) == 2 && size(M0, 1) == d)
+        error('kmeans_ex:invalidarg', 'M0 should be a d x K numeric matrix.');
+    end
+end
+
+if ~(isnumeric(K) && (K >= 1 && K <= n) && K == fix(K))
+    error('kmeans_ex:invalidarg', 'The value of K is invalid.');
+end
+
+% get options
+
+if isempty(varargin)
+    opts = kmeans_ex_set;
+else
+    if isstruct(varargin{1})
+        opts = kmeans_ex_set(varargin{:});
+    else        
+        opts = kmeans_ex_set([], varargin{:});
+    end
+end
     
+dfunc = opts.dist_func;
+costfunc = @(x, y) dfunc(x, y, 'c');
+
+if strcmp(opts.scheme, 'std')
+    use_acc = false;
+else % 'acc'
+    use_acc = true;
+end
 
 
+%% main
 
+% initialize centers
+
+if isempty(M0)    
+    seeds = opts.initFunc(X, K, [], costfunc, opts.rstream);    
+    M0 = X(:, seeds);
+end
+M = M0;
+
+% initialize assignment and status
+
+am = 1:K;
+if use_acc
+    [L, ss, dcc] = reassign_acc(X, M, [], [], [], dfunc);
+else
+    [L, ss, dcc] = reassign_std(X, M, [], dfunc);
+end
+G = intgroup([1, K], L);
+
+tdcc = dcc;
+
+% main loop
+
+it = 0;
+converged = false;
+on_nil_op = opts.on_nil_op;
+
+dispLevel = opts.dispLevel;
+if dispLevel >= 2
+    print_iter_header();    
+    print_iter(it, ss.costs, am, n, dcc);
+end
+
+
+while ~converged && it < opts.max_iter
+    
+    it = it + 1;
+    
+    % update affected mean
+    
+    M_pre = M;
+    [M, any_nil, to_rps] = update_mean(X, M, dfunc, am, G, on_nil_op);
+                
+    if any_nil && on_nil_op > 0   % repick nil centers
+        rpi = find(to_rps);
+               
+        M(:, rpi) = opts.rpickFunc(X, numel(rpi), ss.costs, costfunc, opts.rstream);
+    end
+    
+    % update assignment and status
+    
+    G_pre = G;
+    L_pre = L;
+    
+    if use_acc
+        [L, ss, dcc] = reassign_acc(X, M, M_pre, L, G, dfunc, ss, am);
+    else
+        [L, ss, dcc] = reassign_std(X, M, L, dfunc, ss, am);
+    end
+    G = intgroup([1, K], L);
+    tdcc = tdcc + dcc;
+    
+    % identify affected means
+    
+    am = false(1, K);
+    for k = 1 : K
+        am(k) = ~isequal(G_pre{k}, G{k});
+    end
+    am = find(am);
+    
+    % determine convergence
+    
+    ch = nnz(L ~= L_pre);
+    converged = (ch <= opts.tol_c);
+                
+    % display iteration info
+    if dispLevel >= 2    
+        print_iter(it, ss.costs, am, ch, dcc);
+    end
+end
+
+
+% display final info
+if dispLevel >= 1
+    print_final(it, ss, converged, tdcc);
+end
+
+if ~converged && opts.uc_warn
+    warning('kmeans_ex:unconverged', ...
+        'The K-means procedure did NOT converged.');
+end
+    
+% output info
+if nargout >= 3
+    info.niters = it;
+    info.last_ch = ch;
+    info.converged = converged;
+    
+    info.totalcost = sum(ss.costs);
+end
+
+
+%% core sub-functions
+
+
+function [M, any_nil, to_rps] = update_mean(X, M, dfunc, am, G, on_nil_op)
+% update mean(s) according to new assignment
+% and identify nil mean(s)
+
+any_nil = 0;
+if on_nil_op > 0
+    to_rps = false(1, size(M, 2));
+else
+    to_rps = [];
+end
+
+for k = am    
+    gk = G{k};
+    if ~isempty(gk)
+        M(:, k) = dfunc(X(:, gk), [], 'm');
+    else
+        any_nil = 1;
+        if on_nil_op > 0
+            to_rps(k) = 1;
+        elseif on_nil_op < 0
+            error('kmeans_ex:nilcenter', 'Nil center encountered.');
+        end
+    end
+end
+
+
+function [L, ss, dcc] = reassign_std(X, M, L, dfunc, ss, am)
+% Do cost updating and re-assignment in standard way
+
+n = size(X, 2);
+
+if isempty(L)
+    ss.type = 's';
+    ss.cmat = dfunc(M, X, 'c');
+    dcc = n * size(M, 2);
+else
+    if ~isempty(am)
+        ss.cmat(am, :) = dfunc(M(:, am), X, 'c');
+        dcc = n * numel(am);
+    else
+        dcc = 0;
+    end
+end
+
+[ss.costs, L] = min(ss.cmat, [], 1);
+
+
+function [L, ss, dcc] = reassign_acc(X, M, M_pre, L, G, dfunc, ss, am)
+% Do cost updating and re-assignment in accelerated way
+
+n = size(X, 2);
+K = size(M, 2);
+
+if isempty(L)  % initialize
+    
+    ss.type = 'a';
+    
+    ss.DL = dfunc(M, X, 'd');   % lower-bound of pair-wise distances
+    [ss.d, L] = min(ss.DL, [], 1);  % matching distances
+    ss.costs = dfunc(ss.d, [], 't'); 
+    
+    ss.odated = false(size(ss.DL));  % true when DL(i,j) < dist(i, j)
+        
+    ss.Dm = dfunc(M, M, 'd');   % pairwise distances between means
+    
+    dcc = (n + K) * K;                
+else
+    
+    if ~isempty(am)         
+        
+        % update center distances
+        
+        uDm = dfunc(M(:, am), M, 'd');
+        ss.Dm(am, :) = uDm;
+        ss.Dm(:, am) = uDm';
+        
+        % update lower bound
+        
+        for k = am
+            mov_d = dfunc(M(:,k), M_pre(:,k), 'd');
+            ss.DL(k, :) = max(ss.DL(k, :) - mov_d, 0);
+        end        
+        ss.odated(am, :) = true;    
+                        
+        dcc = numel(am) * (K + 1);
+    else
+        dcc = 0;
+    end
+    
+    % update matching distances
+    
+    d = zeros(1, n);
+    for k = 1 : K
+        gk = G{k};
+        if ~isempty(gk)
+            d_gk = dfunc(M(:, k), X(:, gk), 'd');
+            dcc = dcc + numel(gk);
+            
+            d(gk) = d_gk;
+            ss.DL(k, gk) = d_gk;
+            ss.odated(k, gk) = false;
+        end
+    end
+    
+    % update assignment (and thus new matching distances)
+    
+    for k = 1 : K
+        
+        % identify active candidate for re-assignment
+        
+        dl = ss.DL(k, :);
+        hdc = ss.Dm(k, L) * 0.5;
+        
+        a = find(L ~= k & d > dl & d > hdc);
+
+        if ~isempty(a)
+                                
+            da = ss.DL(k, a);
+            
+            % update active distances
+            
+            oa = ss.odated(k, a);
+            oa_i = a(oa);
+            
+            if ~isempty(oa_i)                                                
+                d_oa = dfunc(M(:, k), X(:, oa_i), 'd');
+                dcc = dcc + numel(oa_i);
+                
+                da(oa) = d_oa;
+                ss.DL(k, oa_i) = d_oa;
+                ss.odated(k, oa_i) = false;
+            end
+            
+            % do re-assignment
+            j = find(da < d(a));            
+            if ~isempty(j)
+                da_j = da(j);
+                j = a(j);
+                
+                L(j) = k;
+                d(j) = da_j;
+            end            
+        end
+                
+    end
+    
+    ss.d = d;    
+    ss.costs = dfunc(ss.d, [], 't');
+end
+
+
+%% Auxiliary functions
+
+
+%% Printing functions
+
+function print_iter_header()
+
+fprintf(' Iter    # af.m.  # ch.ass.     # t.cost   # d.comp \n');
+fprintf('-----------------------------------------------------\n');
+
+
+function print_iter(it, costs, am, ch, dcc)
+
+tcv = sum(costs);
+fprintf('%5d    %7d   %7d  %12.6g  %9d\n', it, numel(am), ch, tcv, dcc);
+
+
+function print_final(it, ss, converged, tdcc)
+
+fprintf('K-means final: total_cost = %.6g, converged = %d [total # iters = %d, # d.comp = %d]\n', ...
+    sum(ss.costs), converged, it, tdcc);
 
 
 
