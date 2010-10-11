98822dcff6aa5f5560131fe6a2afbfd523333286
Dahua Lin <lindahua@gmail.com>
2010-10-10 21:51:28 -0400

    add gr_dfs (tested)

--- graph/common/gr_dfs.m
+++ graph/common/gr_dfs.m
@@ -1,12 +1,11 @@
-function [vs, preds, ford, dtime, ftime] = gr_dfs(G, seeds)
+function [vs, preds, ford] = gr_dfs(G, seeds)
 % Performs DFS traversal
 %
 %   vs = gr_dfs(G, seeds);
 %   [vs, preds] = gr_dfs(G, seeds);
 %   [vs, preds, ford] = gr_dfs(G, seeds);
-%   [vs, preds, ford, dtime, ftime] = gr_dfs(G, seeds);
 %
-%       performs breadth-first-search traversal along the graph G.
+%       performs depth-first-search traversal along the graph G.
 %       G can be an affinity matrix or a mgraph struct.
 %
 %       seeds is an array comprised of the indices of seed nodes.
@@ -18,8 +17,6 @@ function [vs, preds, ford, dtime, ftime] = gr_dfs(G, seeds)
 %       - vs:       the sequence of nodes in visiting order 
 %       - preds:    the predecessors of each nodes
 %       - ford:     the finish order of nodes
-%       - dtime:    the time-stamps of discovery
-%       - ftime:    the time-stamps of finishing
 %
 
 % Created by Dahua Lin, on Oct 3, 2010
@@ -27,7 +24,7 @@ function [vs, preds, ford, dtime, ftime] = gr_dfs(G, seeds)
 
 %% verify input
 
-G = mgraph(G);
+G = gr_adjlist(G);
 
 if ~(isnumeric(seeds) && ~issparse(seeds))
     error('gr_dfs:invalidarg', 'seeds should be a non-sparse numeric array.');
@@ -42,12 +39,10 @@ end
 nout = nargout;
 
 if nout <= 1
-    vs = gr_search_cimp(G, seeds-1, 'd');
+    vs = gr_dfs_cimp(G, seeds-1, 'd');
 elseif nout == 2
-    [vs, preds] = gr_search_cimp(G, seeds-1, 'd');
+    [vs, preds] = gr_dfs_cimp(G, seeds-1, 'd');
 elseif nout == 3
-    [vs, preds, ford] = gr_search_cimp(G, seeds-1, 'd');
-elseif nout <= 5
-    [vs, preds, ford, dtime, ftime] = gr_search_cimp(G, seeds-1, 'd');
+    [vs, preds, ford] = gr_dfs_cimp(G, seeds-1, 'd');
 end
 
