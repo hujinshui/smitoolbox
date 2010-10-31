53b8705dad0f34515d381fab96d7fc59c7cde91f
Dahua Lin <lindahua@gmail.com>
2010-10-31 09:20:30 -0400

    new gr_adjlist and add gr_dump

--- graph/common/gr_adjlist.m
+++ graph/common/gr_adjlist.m
@@ -2,23 +2,31 @@ function G = gr_adjlist(varargin)
 % Construct or verify an adjacency list representation of a graph
 %
 %   In the representation used in smitoolbox, the adjacency list
-%   representation is just a refined version of edgelist, such that
-%   all edges are sorted according to its source vertex. And indexing
-%   information is added to facilitate efficient retrieval of 
-%   adjacent vertices for each vertex.
+%   representation is an augmented version of edgelist, with additional
+%   data structure to represent neighborhood of each vertex.
 %
-%   G = gr_adjlist(G);
-%       if G is a valid adjacency list, it simply returns G, or if
-%       G is an ordinary edge list, it upgrades it to an adjacency list.
+%   G = gr_adjlist(G, dty);
+%       verifies the validity of an adjacency list struct of specified
+%       type, where dty can be 'd' or 'u', respectively indicating
+%       directed or undirected graph.
 %
-%   G = gr_adjlist(A);
+%   G = gr_adjlist(edlist, dty);
+%       constructs a adjacency list from an input edge list.
+%       Here, dty can be 'd' or 'u', indicating whether the graph is
+%       a directed or undirected graph.
+%
+%   G = gr_adjlist(A, dty);
 %       constructs an adjacency list from the adjacency matrix A.
+%       Here, dty can be 'd' or 'u', indicating whether the graph is
+%       a directed or undirected graph.
 %
-%   G = gr_adjlist(n, [s, t]);
-%   G = gr_adjlist(n, [s, t, w]);
-%   G = gr_adjlist(n, s, t);
-%   G = gr_adjlist(n, s, t, w);
+%   G = gr_adjlist(dty, n, [s, t]);
+%   G = gr_adjlist(dty, n, [s, t, w]);
+%   G = gr_adjlist(dty, n, s, t);
+%   G = gr_adjlist(dty, n, s, t, w);
 %       constructs an adjacency list struct from given edges. 
+%       Here, dty can be 'd' or 'u', indicating whether the graph is
+%       a directed or undirected graph.
 %
 %       In the input, n is the number of vertices, and s, t, w (optional)
 %       are vectors of length m. For any i = 1, ..., m, s(i), t(i), and
@@ -29,21 +37,39 @@ function G = gr_adjlist(varargin)
 %   History
 %   -------
 %       - Created by Dahua Lin, on Oct 9, 2010
+%       - Modified by Dahua Lin, on Oct 31, 2010
+%           - the new struct preserves the input order of edges
 %
 
 
 %% main
 
-if nargin == 1 && isstruct(varargin{1})
+if nargin <= 2 && isstruct(varargin{1})
     
-    G = varargin{1};
+    G = varargin{1};  
+    if nargin < 2
+        dty = [];
+    end
     
-    if isfield(G, 'tag')
+    if isfield(G, 'tag')            
+        if strcmp(G.tag, 'gr_adjlist') 
+            if isempty(dty)             
+                return;
+            else
+                if ~isequal(G.dty, dty)
+                    error('gr_adjlist:invalidarg', ...
+                        'The direction type is not as expected.');
+                end
+            end
+                    
         
-        if strcmp(G.tag, 'gr_edgelist')
-            G = gr_adjlist_cimp(G);        
-        elseif strcmp(G.tag, 'gr_adjlist') || strcmp(G.tag, 'gr_bdir_adjlist')
-            return;
+        elseif strcmp(G.tag, 'gr_edgelist')
+            if isempty(dty)
+                error('gr_adjlist:invalidarg', ...
+                    'The direction dtype is not specified.');
+            end
+            G = make_adjlist(G, dty);
+            
         else
             G = [];
         end
@@ -57,10 +83,49 @@ if nargin == 1 && isstruct(varargin{1})
     end
     
 else
-    G = gr_edgelist(varargin{:});    
-    G = gr_adjlist_cimp(G);
+    dty = varargin{1};
+    
+    G = gr_edgelist(varargin{2:end});    
+    G = make_adjlist(G, dty);
     
 end
 
 
+%% The core function for creating adjlist struct
+
+function G = make_adjlist(G, dty)
+
+if isequal(dty, 'd')    
+    [G.o_ds, G.o_os, G.o_es, G.o_ns] = build_nbs(G.n, G.s, G.t);
+    
+elseif isequal(dty, 'u')
+    [G.o_ds, G.o_os, G.o_es, G.o_ns] = build_nbs(G.n, [G.s; G.t], [G.t; G.s]);
+    G.o_es = rem(G.o_es, int32(G.m));
+    
+else
+    error('gr_adjlist:invalidarg', ...
+        'The direction type is invalid.');
+end
+
+G.dty = dty;
+G.tag = 'gr_adjlist';
+
+
+function [ds, os, es, ns] = build_nbs(n, s, t)
+
+[s, es] = sort(s);
+ns = t(es);
+
+p = valueseg(s);
+pds = [diff(p); numel(s)-p(end)+1];
+
+ds = zeros(n, 1);
+ds(s(p)+1) = pds;
+
+os = [0; cumsum(ds(1:end-1))];
+
+ds = int32(ds);
+os = int32(os);
+es = int32(es) - 1;
+ns = int32(ns);
 
---
+++ graph/common/gr_dump.m
@@ -0,0 +1,84 @@
+function gr_dump(G)
+% Dumps a graph
+%
+%   gr_dump(G);
+%
+
+% Created by Dahua Lin, on Oct 31, 2010
+%
+
+%% verify input
+
+if ~(isstruct(G) && isfield(G, 'tag'))
+    error('gr_dump:invalidarg', 'G should be a struct representing a graph.');
+end
+
+if strcmp(G.tag, 'gr_edgelist')
+    is_edgelist = true;
+    is_adjlist = false;
+    
+elseif strcmp(G.tag, 'gr_adjlist')
+    is_edgelist = true;
+    is_adjlist = true;
+    
+else
+    error('gr_dump:invalidarg', 'The input struct is invalid.');
+end
+   
+
+%% main
+
+fprintf('Basic Info:\n');
+fprintf('------------------------\n');
+fprintf('    # nodes = %d\n', G.n);
+fprintf('    # edges = %d\n', G.m);
+fprintf('    # direction = %s\n', direction_name(G));
+fprintf('\n');
+
+if is_edgelist
+    fprintf('Edge List:\n');
+    fprintf('----------------------\n');
+    for i = 1 : G.m
+        fprintf('    [%d]: (%d, %d)\n', i, G.s(i)+1, G.t(i)+1);
+    end
+    fprintf('\n');
+end
+
+if is_adjlist
+    fprintf('Out neighbors:\n');
+    fprintf('----------------------\n');
+    for i = 1 : G.n
+        b = G.o_os(i);
+        d = G.o_ds(i);
+        
+        fprintf('    [%d] (deg = %d): ', i, d);
+        
+        for j = 1 : d
+            fprintf('<%d>-%d ', G.o_es(b+j)+1, G.o_ns(b+j)+1);            
+        end
+        fprintf('\n');
+    end
+    fprintf('\n');
+end
+
+%% subfunctions
+
+function s = direction_name(G)
+
+if isfield(G, 'dty') && ~isempty(G.dty)       
+    switch G.dty
+        case 'd'
+            s = 'directed';
+        case 'u'
+            s = 'undirected';
+        otherwise
+            s = 'INVALID!';
+    end                    
+else
+    s = 'n/a';
+end
+
+
+
+
+
