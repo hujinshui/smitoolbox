3d1e1443d427559f22012cadd9653cef4b538062
Dahua Lin <lindahua@gmail.com>
2010-11-02 16:43:16 -0400

    enrich sgmrf (tested)

--- pmodels/gauss/sgmrf.m
+++ pmodels/gauss/sgmrf.m
@@ -24,6 +24,7 @@ classdef sgmrf
         nedges;     % the number of edges
         graph;      % the underlying graph (gr_adjlist)
         
+        a;          % the regularization coefficients (n x 1 or scalar)
         J;          % the information matrix (n x n sparse)
     end
         
@@ -57,21 +58,70 @@ classdef sgmrf
             g = gr_adjlist(g, 'u');
             if isempty(g.w)
                 error('sgmrf:invalidarg', 'g should be a weighted graph.');
-            end            
+            end                        
             
             % compute J
             
-            J_ = laplacemat(g, a);
+            J_ = laplacemat(g, a); 
+            if size(a, 2) > 1; a = a.'; end
             
             % set fields
             
             obj.nnodes = g.n;
             obj.nedges = g.m;
             obj.graph = g;
-            obj.J = J_;
             
+            obj.a = a;
+            obj.J = J_;            
         end
-    
+        
+        
+        function mu = infer(obj, h)
+            % infer the mean vector from a potential vector
+            %
+            %   mu = obj.infer(h);
+            %       infers the mean vector mu from the potential 
+            %       vector h, by solving J^{-1} h.
+            %
+            %       h can be a column vector of length n or
+            %       multiple vectors organized into an n x K
+            %       matrix. In output, x has the same size as y.
+            
+            mu = obj.J \ h;
+        end
+                
+        function x = smooth(obj, y)
+            % Performs smoothing.
+            %
+            %   x = obj.smooth(y);
+            %       it solves the following problem:
+            %       
+            %           minimize 
+            %           (1/2) sum_i a_i (x_i - y_i)^2 + 
+            %           (1/2) sum_e w_e (x_{e_i} - x_{e_j})^2.
+            %
+            %       y can be a column vector of length n or 
+            %       multiple vectors organized into an n x K matrix.
+            %       In output, x has the same size as y.
+            %
+            
+            n = obj.nnodes;
+            if ~(isfloat(y) && ndims(y) == 2 && size(y,1) == n)
+                error('sgmrf:smooth:invalidarg', ...
+                    'The size of y is invalid.');
+            end
+            
+            a_ = obj.a;                        
+            if size(y,2) == 1 || isscalar(a_)
+                ay = a_ .* y;
+            else
+                ay = bsxfun(@times, a_, y);
+            end
+            
+            x = obj.J \ ay;
+        end
+        
     end
     
 end
+
