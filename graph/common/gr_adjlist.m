989641583f1a9a4cd60d6cc0942b22cf2f3d75b9
Dahua Lin <lindahua@gmail.com>
2010-11-12 23:35:04 -0500

    make gr_adjlist into class (tested)

--- graph/common/gr_adjlist.m
+++ graph/common/gr_adjlist.m
@@ -6,8 +6,7 @@ classdef gr_adjlist < gr_edgelist
     %
     
     
-    properties(GetAccess='public', SetAccess='private')
-        dty;    % direction type ('d': directed, 'u': undirected)
+    properties(GetAccess='public', SetAccess='protected')
         
         o_ds;   % the out-degree of all nodes [n x 1 int32]
         o_os;   % the section offset for all nodes [n x 1 int32 zero-based]
@@ -18,181 +17,236 @@ classdef gr_adjlist < gr_edgelist
     end
     
     
-    
-end
-
-
-
-
-
-
-function G = gr_adjlist(varargin)
-% Construct or verify an adjacency list representation of a graph
-%
-%   In the representation used in smitoolbox, the adjacency list
-%   representation is an augmented version of edgelist, with additional
-%   data structure to represent neighborhood of each vertex.
-%
-%   G = gr_adjlist(G, dty);
-%       verifies the validity of an adjacency list struct of specified
-%       type, where dty can be 'd' or 'u', respectively indicating
-%       directed or undirected graph.
-%
-%       A gr_adjlist struct contains the following fields:
-%       - tag:  a string indicating the type of the graph
-%       - n:    the number of vertices
-%       - m:    the number of edges
-%       - s:    the source vertices of all edges [m' x 1 int32 zero-based]
-%       - t:    the target vertices of all edges [m' x 1 int32 zero-based]
-%       - w:    the edge weights [empty or m' x 1 numeric]
-%       - dty:  the direction type ('d': directed, 'u': undirected)
-%       - o_ds: the out-degree of all nodes [n x 1 int32]
-%       - o_os: the section offset for all nodes [n x 1 int32 zero-based]
-%       - o_es: the concatenated out-going edge array [m' x 1 int32 zero-based]
-%       - o_ns: the concatenated out-going neighbor array [m' x 1 int32 zero-based]
-%
-%       For directed graph, m' = m, while for undirected graph m' = 2m.
-%
-%   G = gr_adjlist(edlist, dty);
-%       constructs a adjacency list from an input edge list.
-%       Here, dty can be 'd' or 'u', indicating whether the graph is
-%       a directed or undirected graph. If dty is omitted, it uses 'd'.
-%
-%   G = gr_adjlist(A, dty);
-%       constructs an adjacency list from the adjacency matrix A.
-%       Here, dty can be 'd' or 'u', indicating whether the graph is
-%       a directed or undirected graph. If dty is omitted, it uses 'd'.
-%
-%   G = gr_adjlist(dty, n, [s, t]);
-%   G = gr_adjlist(dty, n, [s, t, w]);
-%   G = gr_adjlist(dty, n, s, t);
-%   G = gr_adjlist(dty, n, s, t, w);
-%       constructs an adjacency list struct from given edges. 
-%       Here, dty can be 'd' or 'u', indicating whether the graph is
-%       a directed or undirected graph.
-%
-%       In the input, n is the number of vertices, and s, t, w (optional)
-%       are vectors of length m. For any i = 1, ..., m, s(i), t(i), and
-%       w(i) are respectively the source vertex, target vertex, and weight
-%       of the i-th object.
-%
-
-%   History
-%   -------
-%       - Created by Dahua Lin, on Oct 9, 2010
-%       - Modified by Dahua Lin, on Oct 31, 2010
-%           - the new struct preserves the input order of edges
-%
-
-
-%% main
-
-if nargin <= 2 
-    
-    if isstruct(varargin{1})
-    
-        G = varargin{1};  
-        if nargin < 2
-            dty = [];
-        else
-            dty = varargin{2};
+    methods
+        %% Getters
+        
+        function r = out_degrees(G)
+            % Get the outgoing degrees of all nodes
+            %
+            %   G.out_degrees;
+            %
+            
+            r = G.o_ds;
         end
+        
+        
+        function d = out_degree_of(G, i)
+            % Get the outgoing degree of a particular vertex i
+            %
+            %   d = G.out_degree_of(i);
+            %
+            
+            d = G.o_ds(i);            
+        end
+        
+        
+        function r = out_edges_of(G, i)
+            % Get the list of outgoing edges from a particular vertex i
+            %
+            %   G.out_edges_of(i);
+            %
+            
+            b = G.o_os(i);
+            d = G.o_ds(i);            
+            r = G.o_es(b+(1:d)) + 1;            
+        end
+        
+        
+        function [vs, eds] = out_neighbors_of(G, i)
+            % Get the list of outgoing neighbors of a particular vertex i
+            %
+            %   vs = G.out_neighbors_of(i);
+            %   [vs, eds] = G.out_neighbors_of(i);
+            %
+            
+            b = G.o_os(i);
+            d = G.o_ds(i);
+            vs = G.o_ns(b+(1:d)) + 1;
+            if nargout >= 2
+                eds = G.o_es(b+(1:d)) + 1;
+            end            
+        end
+                
+    end
     
-        if isfield(G, 'tag')
-            if strcmp(G.tag, 'gr_adjlist')
-                if isempty(dty)
-                    return;
-                else
-                    if ~isequal(G.dty, dty)
-                        error('gr_adjlist:invalidarg', ...
-                            'The direction type is not as expected.');
+    
+    methods(Static)
+        
+        function G = from_amat(dty, A)
+            % Constructs a graph with adjacency list from affinity matrix
+            %
+            %   G = gr_adjlist.from_amat(dty, A);
+            %
+            %       constructs a graph based on the affinity matrix A.
+            %       Here dty is a char, which can be 'd' or 'u', 
+            %       respectively indicating a directed or undirected
+            %       graph.
+            %
+            
+            edlist = gr_edgelist.from_amat(dty, A);
+            G = gr_adjlist.from_base(edlist);            
+        end
+        
+        
+        function G = from_edges(dty, n, varargin)
+            % Constructs a graph with adjacency list from given edges
+            %
+            %   G = gr_adjlist.from_edges(dty, n, [s, t]);
+            %   G = gr_adjlist.from_edges(dty, n, [s, t, w]);
+            %   G = gr_adjlist.from_edges(dty, n, s, t);
+            %   G = gr_adjlist.from_edges(dty, n, s, t, w);
+            %
+            %       constructs a graph based on the given edges.
+            %       Here, n is the number of vertices, s, t, and w
+            %       are respectively the source vertices, target vertices,
+            %       and weights of edges.
+            %
+            
+            edlist = gr_edgelist.from_edges(dty, n, varargin{:});
+            G = gr_adjlist.from_base(edlist);
+        end
+        
+        
+        function G = from_base(edlist)
+            % Construct a gr_adjlist object from a gr_edgelist object
+            %
+            %   G = gr_adjlist.from_base(edlist);
+            %
+            
+            % get info
+            
+            n = edlist.nv;
+            s0 = edlist.es;
+            t0 = edlist.et;
+            w0 = edlist.ew;
+            
+            if edlist.is_directed
+                s = s0;
+                t = t0;
+                w = w0;
+            else                
+                s = [s0; t0];
+                t = [t0; s0];
+                w = [w0; w0];
+            end            
+            
+            % build neighborhood 
+            
+            if ~isempty(s)            
+                [s, es] = sort(s);
+                ns = t(es);
+                
+                p = valueseg(s);
+                pds = [diff(p); numel(s)-p(end)+1];
+                
+                ds = zeros(n, 1);
+                ds(s(p)+1) = pds;
+                
+                os = [0; cumsum(ds(1:end-1))];
+                
+                ds = int32(ds);
+                os = int32(os);
+                es = int32(es) - 1;
+                ns = int32(ns);
+            else
+                ds = int32([]);
+                os = int32([]);
+                es = int32([]);
+                ns = int32([]);
+            end
+            
+            
+            % set fields
+            
+            G = gr_adjlist();
+            
+            G.dtype = edlist.dtype;            
+            G.nv = edlist.nv;
+            G.ne = edlist.ne;
+            
+            G.es = s;
+            G.et = t;
+            G.ew = w;
+            
+            G.o_ds = ds;
+            G.o_os = os;
+            G.o_es = es;
+            G.o_ns = ns;                
+            
+        end
+                        
+    end
+    
+    
+    
+    
+    methods        
+        
+        %% Info dump
+        
+        function dump(G)
+            % dump information about the graph
+            %
+            %   dump(G);
+            %
+            
+            n = G.nv;
+            m = G.ne;
+            
+            if G.is_directed
+                fprintf('Directed Adjacency List: \n');
+            else
+                fprintf('Undirected Adjacency List: \n');
+            end
+            fprintf('------------------------\n');
+            fprintf('    # nodes = %d\n', n);
+            fprintf('    # edges = %d\n', m);            
+            fprintf('\n');
+                        
+            fprintf('  edges: \n');
+            s = G.source_vs;
+            t = G.target_vs;
+            if ~G.is_weighted                
+                for i = 1 : m
+                    fprintf('    [%d]: (%d, %d)\n', i, s(i), t(i));
+                end
+            else
+                w = G.weights;
+                for i = 1 : m
+                    fprintf('    [%d]: (%d, %d) = %g\n', i, s(i), t(i), w(i));
+                end
+            end
+            fprintf('\n');
+            
+            fprintf('  out neighbors:\n');
+            for i = 1 : n
+                d = G.out_degree_of(i);
+                [nvs, nes] = G.out_neighbors_of(i);
+                
+                fprintf('    [%d] (deg = %d): ', i, d);
+                
+                for j = 1 : d
+                    v = nvs(j);
+                    e = nes(j);
+                    
+                    if e <= m
+                        fprintf('%d>%d ', e, v);
+                    else
+                        fprintf('%d<%d ', e-m, v);
                     end
                 end
-                
-                
-            elseif strcmp(G.tag, 'gr_edgelist')
-                if isempty(dty)
-                    dty = 'd';
-                end
-                G = make_adjlist(G, dty);
-                
-            else
-                G = [];
+                fprintf('\n');
             end
-        else
-            G = [];
+            fprintf('\n');
+            
         end
+                
         
-        if isempty(G)
-            error('gr_adjlist:invalidarg', ...
-                'G is not a valid struct for graph representation.');
-        end
+    end
+      
     
-    elseif isnumeric(varargin{1})
-        
-        A = varargin{1};
-        if nargin < 2
-            dty = 'd';
-        else
-            dty = varargin{2};
-        end
-                    
-        G = gr_edgelist(A, dty);
-        G = make_adjlist(G, dty);
-    end                
-    
-else
-        
-    dty = varargin{1};
-    
-    G = gr_edgelist(varargin{2:end});    
-    G = make_adjlist(G, dty);
     
 end
 
 
-%% The core function for creating adjlist struct
-
-function G = make_adjlist(G, dty)
-
-if isequal(dty, 'd')    
-    [G.o_ds, G.o_os, G.o_es, G.o_ns] = build_nbs(G.n, G.s, G.t);
-    
-elseif isequal(dty, 'u')
-    s = G.s;
-    t = G.t;
-    w = G.w;
-    
-    G.s = [s; t];
-    G.t = [t; s];
-    G.w = [w; w];
-    
-    [G.o_ds, G.o_os, G.o_es, G.o_ns] = build_nbs(G.n, G.s, G.t);        
-    
-else
-    error('gr_adjlist:invalidarg', ...
-        'The direction type is invalid.');
-end
-
-G.dty = dty;
-G.tag = 'gr_adjlist';
-
-
-function [ds, os, es, ns] = build_nbs(n, s, t)
-
-[s, es] = sort(s);
-ns = t(es);
-
-p = valueseg(s);
-pds = [diff(p); numel(s)-p(end)+1];
-
-ds = zeros(n, 1);
-ds(s(p)+1) = pds;
-
-os = [0; cumsum(ds(1:end-1))];
-
-ds = int32(ds);
-os = int32(os);
-es = int32(es) - 1;
-ns = int32(ns);
 
