e396ee04e5efce0a8a4e90b12aaec5e94ae14a30
Dahua Lin <lindahua@gmail.com>
2010-11-13 01:34:22 -0500

    make gr_tree to class

--- graph/common/gr_tree.m
+++ graph/common/gr_tree.m
@@ -1,79 +1,165 @@
-function T = gr_tree(varargin)
-% Constructs a directed tree/forest structure
-%
-%   Each tree/forest is represented by a struct with the following 
-%   fields:
-%   - tag:      the tag indicating the type of the struct ('gr_tree')
-%   - n:        the number of vertices
-%   - ntr:      the number of trees (if the entire graph is connected, K = 1)
-%   - m:        the number of edges (n - ntr)
-%   - rs:       the roots [1 x ntr int32 zero-based]
-%   - ps:       the parents of each node [n x 1 int32 zero-based]
-%   - cs:       the concatenated array of children [m x 1 int32 zero-based]
-%   - ncs:      the number of childrens of each node [n x 1 int32]
-%   - os:       the offsets of each node for cs [n x 1 int32 zero-based]
-%
-%   T = gr_tree(parents);
-%       constructs a tree/forest by specifying the parents of all nodes.
-%       
-%       Suppose there are n nodes, then parents should be a vector 
-%       of length n, where parents(i) is the parent of the i-th node.
-%       For root nodes, their parents are zeros.
-%
-
-% Created by Dahua Lin, on Oct 31, 2010
-%
-
-%% main
-
-if nargin == 1 && isnumeric(varargin{1})
-    parents = varargin{1};
-
-    T = from_parents(parents);    
+classdef gr_tree
+    % The class to represent a tree (in graph theoretical sense)
+    %
     
-else
-    error('gr_tree:invalidarg', ...
-        'The input arguments are invalid.');
+    % Created by Dahua Lin, on Nov 13, 2010
+    %
+   
+    
+    properties(GetAccess='public', SetAccess='protected')
+        
+        nv;     % the number of vertices
+        ne;     % the number of edges (nv - ntr)
+        ntr;    % the number of connected trees
+        
+        rts;    % the roots [1 x ntr int32 zero-based]
+        pas;    % the parents [n x 1 int32 zero-based]
+        
+        chds;   % the concatenated array of children [ne x 1 int32 zero-based]
+        nchs;   % the number of children of each node [n x 1 int32]
+        ofs;    % the offsets of each node in chds [n x 1 int32 zero-based]        
+    end
+    
+    
+    methods
+        
+        function rs = roots(T)
+            % Get the root vertices
+            %
+            %   T.roots;
+            %
+            
+            rs = T.rts + 1;            
+        end
+        
+        
+        function ps = parents(T)
+            % Get the array of all parents
+            %
+            %   T.parents;
+            %
+            
+            ps = T.pas + 1;
+        end
+                
+        
+        function p = parent_of(T, i)
+            % Get the parent of a particular vertex i
+            %
+            %   T.parent_of(i);
+            %
+            
+            p = T.pas(i) + 1;
+        end
+        
+        
+        function cs = children_of(T, i)
+            % Get the children vertices of a particular vertex i
+            %
+            %   T.children_of(i);
+            %
+            
+            cs = T.chds(T.ofs(i)+(1:T.nchs(i))) + 1;
+        end
+                        
+    end
+    
+    
+    methods(Static)
+       
+        function T = from_parents(pas)
+            % Construct a tree from parents of all vertices
+            %
+            %   T = gr_tree.from_parents(parents);
+            %
+            
+            if ~(isnumeric(pas) && isvector(pas))
+                error('gr_tree:invalidarg', 'parents should be a vector.');
+            end
+            
+            n = numel(pas);
+            
+            if issparse(pas); pas = full(pas); end
+            if size(pas, 2) > 1; pas = pas.'; end
+            
+            rs = find(pas == 0);
+            
+            if isempty(rs)
+                error('gr_tree:rterror', 'No root is found in the tree.');
+            end
+            nt = numel(rs);
+            
+            [sp, cs] = sort(pas); %#ok<ASGLU>
+            cs = cs(nt+1:end);
+            
+            ncs = intcount([1, n], pas).';
+            os = [0; cumsum(ncs(1:end-1))];
+            
+            % set fields
+            
+            T = gr_tree();
+            
+            T.nv = n;
+            T.ne = n - nt;
+            T.ntr = nt;
+            
+            T.rts = int32(rs) - 1;
+            T.pas = int32(pas) - 1;
+            
+            T.chds = int32(cs) - 1;
+            T.nchs = int32(ncs);
+            T.ofs = int32(os);
+            
+        end
+                                
+    end
+    
+    
+    methods
+       
+        function dump(T)
+            % Dump the tree
+            %
+            %   dump(T);
+            %
+            
+            n = T.nv;
+            
+            fprintf('Tree \n');
+            fprintf('------------------------\n');
+            fprintf('    # nodes = %d\n', n);
+            fprintf('    # comps = %d\n', T.ntr);
+            fprintf('    # edges = %d\n', T.ne);            
+            fprintf('\n');
+            
+            fprintf('  nodes:\n');
+            
+            for i = 1 : n
+                nc = T.nchs(i); 
+                pa = T.parent_of(i);
+                fprintf('    [%d] (pa = %d)', i, pa);
+                
+                if nc > 0
+                    fprintf(' ==> ');
+                    cs = T.children_of(i);
+                    for j = 1 : nc                        
+                        fprintf('%d ', cs(j));
+                    end
+                end
+                fprintf('\n');
+            end
+            fprintf('\n');
+            
+        end
+        
+        
+    end
+    
+    
+    
 end
 
 
-%% core functions
-
-function T = from_parents(parents)
-
-if ~(isnumeric(parents) && isvector(parents))
-    error('gr_tree:invalidarg', 'parents should be a vector.');
-end
-
-n = numel(parents);
-
-if issparse(parents); parents = full(parents); end
-if size(parents, 2) > 1; parents = parents.'; end
-
-roots = find(parents == 0);
-
-if isempty(roots)
-    error('gr_tree:rterror', 'No root is found in the tree.');
-end
-ntr = numel(roots);
-
-[sp, cs] = sort(parents); %#ok<ASGLU>
-cs = cs(ntr+1:end);
-
-ncs = intcount([1, n], parents).';
-os = [0; cumsum(ncs(1:end-1))];
-
-T = struct( ...
-    'tag', 'gr_tree', ...
-    'dty', 'd', ...
-    'n', n, ...
-    'ntr', ntr, ...
-    'm', n - ntr, ...
-    'rs', int32(roots) - 1, ...
-    'ps', int32(parents) - 1, ...
-    'cs', int32(cs) - 1, ...
-    'ncs', int32(ncs), ...
-    'os', int32(os));
 
 
 
