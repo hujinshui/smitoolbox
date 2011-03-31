c5ae9584408ea4f6faf040cd7542b82f12ff4edb
Dahua Lin <lindahua@gmail.com>
2011-03-31 19:04:33 -0400

    reimplement gr_dijkstra using bcslib (tested)

--- graph/common/gr_dijkstra.m
+++ graph/common/gr_dijkstra.m
@@ -1,19 +1,21 @@
-function [vs, dists, preds] = gr_dijkstra(G, s)
+function [dists, preds] = gr_dijkstra(G, s)
 % Solve single-source shortest path problem using Dijkstra's algorithm
 %
-%   [vs, dists, preds] = gr_dijkstra(G, s);
+%   [dists, preds] = gr_dijkstra(G, s);
 %       solves the single-source shortest-path problem with source s,
 %       using Dijkstra's algorithm.       
 %
 %       Output arguments:
-%       - vs:       the sequence of accessible nodes in the order of closing
-%       - dists:    the corresponding distances from source
-%       - preds:    the corresponding parents
+%       - dists:    the distances from source [1 x nv weight_type]
+%       - preds:    the parents of each node [1 x nv int32]
+%                   The parent of the source node is set to 0,
+%                   and that of the non-visited node is set to -1.
 %
 
 %   History
 %   -------
 %       - Created by Dahua Lin, on Oct 10, 2010
+%       - Modified by Dahua Lin, on Mar 31, 2011
 %
 
 %% verify input
@@ -28,5 +30,13 @@ end
 
 %% main
 
-[vs, preds, dists] = gr_dijkstra_cimp(G, int32(s-1));
+[dists, preds] = gr_dijkstra_cimp(G, int32(s-1));
+
+preds(s) = 0;
+un_visited = preds == (1:G.nv);
+if any(un_visited)
+    preds(un_visited) = -1;
+    dists(un_visited) = inf;
+end
+
 
