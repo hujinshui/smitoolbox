ff6b4c72e4f34e8b44553de62610a5a2da5c6322
Dahua Lin <lindahua@gmail.com>
2011-08-30 15:32:23 -0400

    new smi framework done & tested

--- frmwork/private/smi_allocate_struct.m
+++ frmwork/private/smi_allocate_struct.m
@@ -1,8 +1,19 @@
 function S = smi_allocate_struct(fnames, m, n)
 % allocate a struct array with specified fieldnames 
 
+if nargin < 2
+    m = 1;
+end
+
+if nargin < 3
+    n = 1;
+end
+
 nf = numel(fnames);
 args = cell(2, nf);
 args(1, :) = fnames;
 
-S = repmat(struct(args{:}), m, n);
+S = struct(args{:});
+if m ~= 1 || n ~= 1
+    S = repmat(S, m, n);
+end
--- frmwork/smi_gibbs_sample.m
+++ frmwork/smi_gibbs_sample.m
@@ -1,14 +1,15 @@
-function R = smi_gibbs_sample(frmwork, opts, initcfgs)
+function R = smi_gibbs_sample(program, opts, initcfgs)
 % Performs (generalized) Gibbs sampling over a smi framework
 %
-%   R = smi_gibbs_sample(frmwork, opts, initcfgs);
+%   R = smi_gibbs_sample(program, opts);
+%   R = smi_gibbs_sample(program, opts, initcfgs);
 %
 %       Performs Gibbs sampling over a statistical modeling and inference
 %       framework. 
 %
 %       Input arguments:
-%       - frmwork:      The framework upon which sampling is performed.
-%                       It should be an object of the class smi_frmwork.
+%       - program:      An SMI program upon which the sampling is 
+%                       performed.
 %
 %       - opts:         The options that control the sampling procedure.
 %                       This can be made by calling gs_options.
@@ -22,6 +23,9 @@ function R = smi_gibbs_sample(frmwork, opts, initcfgs)
 %                       If one wants to run multiple MCMC chains in 
 %                       parallel, initcfgs can be a struct array, with 
 %                       each element providing an initial configuration.
+%
+%                       If proper initialization steps have been added
+%                       to the program, then initcfgs can be omitted.
 %
 %       Output arguments:
 %       - R:            The cell array comprised of collected samples. 
@@ -48,63 +52,76 @@ function R = smi_gibbs_sample(frmwork, opts, initcfgs)
 
 %% Verify input arguments
 
-if ~isa(frmwork, 'smi_frmwork')
+if ~isa(program, 'smi_program')
     error('smi_gibbs_sample:invalidarg', ...
-        'frmwork should be an object of class smi_frmwork.');
+        'program should be an object of class smi_program.');
+end
+
+bmap = program.block_map;
+if ~isfield(bmap, 'default')
+    error('smi_gibbs_sample:invalidarg', ...
+        'default block is lacking in the program.');
+end
+
+blk_default = program.blocks(bmap.default);
+
+blk_init = [];
+if isfield(bmap, 'init')
+    blk_init = program.blocks(bmap.init);
 end
 
 opts = gs_options(opts);
 nr = opts.nrepeats;
 
-if ~isstruct(initcfgs)
-    error('smi_gibbs_sample:invalidarg', 'initcfgs should be a struct.');
+if nargin < 3
+    initcfgs = [];
+else
+    if ~isstruct(initcfgs)
+        error('smi_gibbs_sample:invalidarg', 'initcfgs should be a struct.');
+    end        
 end
 n0 = numel(initcfgs);
 
-if n0 == 1    
-    repr = preprocess_cfg(frmwork, initcfgs);
-    
+
+%% Main 
+
+if n0 <= 1    
+        
     if nr == 1  % single config, single run each
-        R = do_sim(frmwork, opts, repr, 0);
+        R = do_sim(program, blk_default, blk_init, opts, initcfgs, 0);
         R = { R };
         
     else        % single config, multi run each
         R = cell(nr, 1);
         
         parfor i = 1 : nr
-            R{i} = do_sim(frmwork, opts, repr, i);
+            R{i} = do_sim(program, blk_default, blk_init, opts, initcfgs, i);
         end
     end
     
 else            
     if nr == 1  % multiple configs, single run each
-        
-        reprs = cell(1, n0);
-        for j = 1 : n0
-            reprs{j} = preprocess_cfg(frmwork, initcfgs(j));
-        end
-        
+                
         R = cell(1, n0);
         
         parfor i = 1 : n0
-            R{i} = do_sim(frmwork, opts, reprs{i}, i);
+            R{i} = do_sim(program, blk_default, blk_init, opts, initcfgs(i), i);
         end        
         
     else        % multiple configs, multi run each
         
-        reprs = cell(nr, n0);
+        Cfgs = cell(nr, n0);
         for j = 1 : n0
-            repr = preprocess_cfg(frmwork, initcfgs(j));            
             for i = 1 : nr
-                reprs{i, j} = repr;
-            end            
+                Cfgs{i, j} = initcfgs(j);
+            end
         end
         
         R = cell(nr, n0);                
         
         N = nr * n0;
         parfor i = 1 : N
-            R{i} = do_sim(frmwork, opts, reprs{i}, i);
+            R{i} = do_sim(program, blk_default, blk_init, opts, Cfgs{i}, i);
         end
     end
     
@@ -113,164 +130,105 @@ end
 
 %% core simulation procedure
 
-function S = do_sim(frmwork, opts, repr, ithread)
+function S = do_sim(program, blk_default, blk_init, opts, cfg0, ithread)
 % single run with a single config
 
-% extract fields
+% establish context
 
-seq0 = frmwork.init_seq;
-len0 = numel(seq0);
+if ~opts.check
+    ctx = smi_exec_context(program);
+else
+    ctx = smi_exec_context(program, 'check');
+end
 
-cseq = frmwork.cycle_seq;
-clen = numel(cseq);
-
-fs = frmwork.funcs;
-insts0 = frmwork.instructions0;
-insts = frmwork.instructions;
-vmap = frmwork.var_map;
-
-T0 = opts.burnin;
-N = opts.nsamples;
-nc = opts.cps;
 displevel = opts.display;
 
-if displevel >= 1
-    if ithread > 0
-        dprefix = ['[[thread ' int2str(ithread), ']] '];
-    else
-        dprefix = '';
-    end
-end
-
-% get ids of variable to be output to samples
-if isempty(opts.output_vars)
-    ovars = {frmwork.vars.name};
-else
-    ovars = opts.output_vars;
-end
-ovar_ids = zeros(size(ovars));
-for i = 1 : numel(ovars)
-    ovar_ids(i) = vmap.(ovars{i});
-end
-
-% initialize result container
-S = cell(1, N);
+% initialization
 
 if displevel >= 2
-    disp([dprefix, 'simulation started']);
+    fprintf('[[thread %d]] initializing ...\n', ithread);
 end
 
-% initialization 
+if ~isempty(cfg0)
+    ctx.set_vars(cfg0);
+end
 
-if len0 > 0
-    for i = 1 : len0
+if ~isempty(blk_init)
+    exec_block(ctx, blk_init);
+end
 
-        % pick the current step
-        st = insts0(i);
-        f = fs(st.f_id).func;
+% run sampling
 
-        if displevel >= 4
-            disp([dprefix, sprintf('    step %d: %s', ...
-                i, fs(st.f_id).name)]);
-        end
+nsamples = opts.nsamples;
+S = cell(nsamples, 1);
 
-        % form inputs
-        vin = smi_from_repr(repr, st.v_in);
-
-        % do update computation
-        vout = cell(1, st.n_out);
-        [vout{:}] = f.evaluate(st.out_flags, vin{:});
-
-        % put the results back to repr
-        for j = 1 : st.n_out
-            if st.v_out(j) > 0
-                repr{st.v_out(j)} = vout{j};
-            end
-        end
-    end
-
+if displevel < 4
+    
+    % burn in
+    
     if displevel >= 2
-        disp([dprefix, 'initialization finished']);
+        fprintf('[[thread %d]] burning in ...\n', ithread);
+    end    
+    
+    exec_block(ctx, blk_default, opts.burnin);
+    
+    % main iterations
+    
+    if displevel >= 2
+        fprintf('[[thread %d]] collecting samples ...\n', ithread);
     end
+    
+    for i = 1 : nsamples
+        exec_block(ctx, blk_default, opts.cps);
+        S{i} = ctx.extract_vars(opts.output_vars);
+        
+        if displevel >= 3
+            fprintf('[[thread %d]]   %d/%d samples collected.\n', ...
+                ithread, i, nsamples);
+        end
+    end
+    
+else % displevel >= 4 (iters)
+    
+    % burn in
+    
+    fprintf('[[thread %d]] burning in ...\n', ithread);
+    
+    for t = 1 : opts.burnin
+        fprintf('[[thread %d]]     burn-in iter %d/%d\n', ...
+            ithread, t, opts.burnin);
+        
+        exec_block(ctx, blk_default);       
+    end
+    
+    % main iterations
+    
+    fprintf('[[thread %d]] collecting samples ...\n', ithread);
+    
+    cps = opts.cps;
+    for i = 1 : nsamples
+        for t = 1 : cps
+            fprintf('[[thread %d]]     sample[%d] iter %d/%d\n', ...
+                ithread, i, t, cps);
+            
+            exec_block(ctx, blk_default);
+        end
+        S{i} = ctx.extract_vars(opts.output_vars);
+        
+        fprintf('[[thread %d]]   %d/%d samples collected.\n', ...
+            ithread, i, nsamples);        
+    end
+    
 end
 
+% combine collected samples
 
-% main loop
-Tstart = -T0;
-Tend = (nc - 1) * N;
-
-c = 0; % sample counter            
-for t = Tstart : Tend
-
-    if t == 0 && displevel >= 2
-        disp([dprefix, 'burn-in finished']);
-    end
-
-    if displevel >= 3                    
-        disp([dprefix, sprintf('  cycle %d ...', t)]);
-    end
-
-    % do update cycle
-    for i = 1 : clen 
-
-        % pick the current step
-        st = insts(i);
-        f = fs(st.f_id).func;                  
-
-        if displevel >= 4
-            disp([dprefix, sprintf('    step %d: %s', ...
-                i, fs(st.f_id).name)]);
-        end
-
-        % form inputs                                        
-        vin = smi_from_repr(repr, st.v_in);
-
-        % do update computation
-        vout = cell(1, st.n_out);
-        [vout{:}] = f.evaluate(st.out_flags, vin{:});
-
-        % put the results back to repr
-        for j = 1 : st.n_out
-            if st.v_out(j) > 0
-                repr{st.v_out(j)} = vout{j};
-            end
-        end
-    end       
-
-    % collect sample                
-    if t >= 0 && mod(t, nc) == 0
-        c = c + 1;                    
-        for i = 1 : numel(ovars)
-            sp.(ovars{i}) = repr{ovar_ids(i)};
-        end
-        S{c} = sp;
-    end
-end
-
-% convert results to struct form
-S = vertcat(S{:});  
+S = vertcat(S{:});
 
 if displevel >= 1
-    disp([dprefix, 'simulation done']);
+    fprintf('[[thread %d]] finished.\n', ithread);
 end
 
 
 
-%% auxiliary functions
-
-function repr = preprocess_cfg(frmwork, cfg)
-% form internal representation and verify
-        
-nv = frmwork.num_vars;
-vs = frmwork.vars;
-
-% form internal representation
-repr = cell(1, nv);
-for i = 1 : nv
-    vn = vs(i).name;
-    if isfield(cfg, vn)
-        repr{i} = cfg.(vn);
-    end
-end
     
-
