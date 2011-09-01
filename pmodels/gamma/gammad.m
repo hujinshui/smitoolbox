ac2f6479a6385ed71a85bb9af9ed243606b52a50
Dahua Lin <lindahua@gmail.com>
2011-09-01 01:07:36 -0400

    gammad written (not tested)

--- pmodels/gamma/gammad.m
+++ pmodels/gamma/gammad.m
@@ -5,8 +5,12 @@ classdef gammad
     %   distributions. A Gamma distribution is characterized by two
     %   parameters:
     %
-    %   - shape:    the shape parameter 
-    %   - scale:    the scale parameter
+    %   - alpha:    the shape parameter 
+    %   - beta:     the scale parameter (inverse of scale)
+    %
+    %   The pdf is given by
+    %
+    %   f(x) = (alpha - 1) log(x) - beta x 
     %  
     %   For an object with n distributions over d-dimensional space:    
     %   - shape is a matrix of size 1 x n or d x n.
@@ -20,8 +24,12 @@ classdef gammad
         dim;    % the dimension of the underlying space
         num;    % the number of distributions contained in the object
         
-        shape;  % the shape parameter(s)
-        scale;  % the scale parameter(s)
+        alpha;  % the shape parameter(s)
+        beta;   % the scale parameter(s)
+        
+        lpconst;   % a constant term in logpdf
+                   % = alpha * log(beta) + gammaln(alpha)
+                   % 1 x n row vector or 0.
     end
         
     methods
@@ -35,21 +43,21 @@ classdef gammad
             %       to the i-th distribution in obj.
             %
         
-            k = obj.shape;
-            sc = obj.scale;
+            a = obj.alpha;
+            b = obj.beta;
             d = obj.dim;
             
             if isscalar(sc)
                 if sc == 1
-                    v = k;
+                    v = a;
                 else
-                    v = k * sc;
+                    v = a * b;
                 end
             else
-                v = bsxfun(@times, k, sc);
+                v = bsxfun(@times, a, b);
             end
             
-            if size(k, 1) ~= d
+            if size(a, 1) ~= d
                 v = v(ones(d, 1), :);
             end                
         end
@@ -63,21 +71,21 @@ classdef gammad
             %       to the i-th distribution in obj.
             %
             
-            k = obj.shape;
-            sc = obj.scale;
+            a = obj.alpha;
+            b = obj.beta;
             d = obj.dim;
             
-            if isscalar(sc)
+            if isscalar(b)
                 if sc == 1
-                    v = obj.shape;
+                    v = a;
                 else
-                    v = (sc^2) * obj.shape;
+                    v = a * (b^2);
                 end
             else
-                v = bsxfun(@times, obj.shape, (sc.^2));
+                v = bsxfun(@times, a, (b.^2));
             end
             
-            if size(k, 1) ~= d
+            if size(a, 1) ~= d
                 v = v(ones(d, 1), :);
             end
         end
@@ -92,16 +100,16 @@ classdef gammad
             %       to the i-th distribution in obj.
             %            
             
-            k = obj.shape;
-            sc = obj.scale;
+            a = obj.alpha;
+            b = obj.beta;
             d = obj.dim;
             
-            v = k + gammaln(k) + (1 - k) .* psi(k);
+            v = k + gammaln(a) + (1 - a) .* psi(a);
             if ~isequal(sc, 1)
                 if isscalar(sc) 
-                    v = v + log(sc);
+                    v = v + log(b);
                 else
-                    v = bsxfun(@plus, v, log(sc));
+                    v = bsxfun(@plus, v, log(b));
                 end
             end
             
@@ -120,23 +128,23 @@ classdef gammad
     
     methods 
         
-        function obj = gammad(shape, scale, d)
+        function obj = gammad(alpha, beta, d)
             % Constructs a Gamma distribution object
             %
-            %   obj = gammad(shape);
-            %   obj = gammad(shape, scale);
+            %   obj = gammad(alpha);
+            %   obj = gammad(alpha, beta);
             %
             %       constructs a Gamma distribution object given 
             %       the parameters.
             %
             %       Inputs:
-            %       - shape:    the shape parameter of size d x n.
+            %       - alpha:    the shape parameter of size d x n.
             %
-            %       - scale:    the scale parameter, which can be
+            %       - beta:     the scale parameter, which can be
             %                   either a 1 x n row vector or a 
             %                   scale (if shared by all distributions).
             %
-            %   obj = gammad(shape, scale, d);
+            %   obj = gammad(alpha, beta, d);
             %   
             %       To construct multi-dimensional gamma distributions,
             %       where each dimension has the same shape param, then
@@ -148,16 +156,16 @@ classdef gammad
                         
             % verify inputs
             
-            if ~(isfloat(shape) && ndims(shape) == 2)
+            if ~(isfloat(alpha) && ndims(alpha) == 2)
                 error('gammad:invalidarg', ...
-                    'shape should be a numeric matrix.');
+                    'alpha should be a numeric matrix.');
             end            
-            [d_, n] = size(shape);
+            [d_, n] = size(alpha);
             
-            if ~( isfloat(scale) && (isscalar(scale) || ...
-                    isequal(size(scale), [1 n])) )
+            if ~( isfloat(beta) && (isscalar(beta) || ...
+                    isequal(size(beta), [1 n])) )
                 error('gammad:invalidarg', ...
-                    'scale should be a scalar or a 1 x n row vector.');
+                    'beta should be a scalar or a 1 x n row vector.');
             end
             
             if nargin < 3
@@ -173,20 +181,240 @@ classdef gammad
                 end
             end
             
+            % compute constant terms
+            
+            if isequal(beta, 1)
+                c1 = 0;
+            else
+                if d == 1
+                    sa = alpha;
+                else
+                    if size(alpha, 1) == 1
+                        sa = alpha * d;
+                    else
+                        sa = sum(alpha, 1);
+                    end
+                end
+                c1 = sa * log(beta);
+            end
+            
+            if isequal(alpha, 1)
+                c2 = 0;
+            else
+                c2 = gammaln(alpha);
+                if d > 1
+                    if size(alpha, 1) == 1
+                        c2 = c2 * d;
+                    else
+                        c2 = sum(c2, 1);
+                    end
+                end
+            end   
+                        
+            % create object
+            
             obj.dim = d;
             obj.num = n;
-            obj.shape = shape;
-            obj.scale = scale;
+            obj.alpha = alpha;
+            obj.beta = beta;
+            obj.lpconst = c1 + c2;
         end            
     end
     
     
     %% Evaluation
     
+    methods
+       
+        function L = logpdf(obj, X, si)
+            % Evaluates log-pdf of given samples
+            %
+            %   L = obj.logpdf(X);
+            %
+            %       computes the logarithm of pdf at the samples given
+            %       as columns of X.
+            %
+            %       Suppose there are m distributions in obj, and n 
+            %       columns in X, then L is a matrix of size m x n, 
+            %       where L(k, i) is the log-pdf of the i-th sample
+            %       with respect to the k-th distribution.
+            %
+            %   L = obj.logpdf(X, si);
+            %
+            %       computes the logarithm of pdf with respect to the
+            %       distributions selected by si.
+            %
+            
+            d = obj.dim;
+            if ~(isfloat(X) && ndims(X) == 2 && size(X,1) == d)
+                error('gammad:invalidarg', ...
+                    'X should be a numeric matrix with size(X,1) == dim.');
+            end
+            
+            a = obj.alpha;
+            b = obj.beta;
+            lpc = obj.lpconst;
+            
+            if nargin >= 3 && ~isempty(si)
+                a = a(:, si);
+                if ~isscalar(b)
+                    b = b(:, si);
+                end
+            end
+            
+            if isequal(a, 1)
+                T1 = 0;
+            else
+                if size(a, 1) == d
+                    T1 = (a - 1)' * log(X);
+                else
+                    T1 = (a - 1)' * sum(log(X), 1);
+                end
+            end
+            
+            if d == 1
+                sX = X;
+            else
+                sX = sum(X, 1);
+            end
+            if isequal(b, 1)
+                T2 = sX;
+            else
+                T2 = (1./b)' * X;
+            end
+            
+            L = T1 - T2;
+            if ~isequal(lpc, 0)
+                L = bsxfun(@minus, L, lpc.');                                
+            end                        
+        end
+        
+        
+        function L = pdf(obj, X, si)
+            % Evaluates log-pdf of given samples
+            %
+            %   L = obj.pdf(X);
+            %
+            %       computes the pdf values at the samples given
+            %       as columns of X.
+            %
+            %       Suppose there are m distributions in obj, and n
+            %       columns in X, then L is a matrix of size m x n,
+            %       where L(k, i) is the log-pdf of the i-th sample
+            %       with respect to the k-th distribution.
+            %
+            %   L = obj.pdf(X, si);
+            %
+            %       computes the pdf values with respect to the
+            %       distributions selected by si.
+            %
+            
+            if nargin < 3
+                L = exp(logpdf(obj, X));
+            else
+                L = exp(logpdf(obj, X, si));
+            end            
+        end
+                
+    end    
     
     %% Sampling
     
+    methods
+        
+        function X = sample(obj, n, i) 
+            % Samples from the Gamma distribution
+            %            
+            %   X = obj.sample();
+            %   X = obj.sample(n);
+            %       draws n samples from the Gamma distribution. 
+            %       (It must be obj.num == 1 for this syntax).
+            %
+            %       When n is omitted, it is assumed to be 1.
+            %
+            %   X = obj.sample(n, i);
+            %       draws n samples from the i-th Gamma distribution.            
+            %
+            %       When i is a vector, then n can be a vector of the 
+            %       same size. In this case, it draws n(j) samples from
+            %       the i(j)-th distribution.                     
+            %
+            
+            if nargin < 2; n = 1; end
+            
+            a = obj.alpha;
+            b = obj.beta;
+            d = obj.dim;
+            
+            if nargin < 3 || isempty(i)
+                if G.num > 1
+                    error('gammad:sample:invalidarg', ...
+                        'i is needed when obj contains multiple models.');
+                end
+                
+                if isscalar(a)
+                    X = randg(a, d, n);
+                else
+                    if n == 1
+                        X = randg(a);
+                    else
+                        X = randg(a(:, ones(1, n)));
+                    end
+                end
+                
+                if b ~= 1
+                    X = b * X;
+                end
+                
+            else
+                if ~(isvector(n) && isnumeric(n))
+                    error('gammad:pos_sample:invalidarg', ...
+                        'n must be a numeric vector');
+                end
+                if ~(isvector(i) && isnumeric(i))
+                    error('gammad:pos_sample:invalidarg', ...
+                        'i must be a numeric vector');
+                end
+                if numel(n) ~= numel(i)
+                    error('gammad:pos_sample:invalidarg', ...
+                        'The sizes of n and i are inconsistent.');
+                end
+                
+                a = a(:, i);
+                K = size(a, 2);
+                
+                if K == 1
+                    if isscalar(a)
+                        X = randg(a, d, n);
+                    else
+                        X = randg(a(:, ones(1, n)));
+                    end
+                else
+                    
+                    N = sum(n);
+                    X = zeros(d, N, class(a));
+                    ek = 0;
+                    
+                    if size(a, 1) == 1
+                        for k = 1 : K
+                            sk = ek + 1;
+                            ek = ek + n(k);
+                            X(:, sk:ek) = randg(a(k), d, n(k));
+                        end           
+                    else
+                        for k = 1 : K
+                            sk = ek + 1;
+                            ek = ek + n(k);
+                            X(:, sk:ek) = randg(a(:, ones(1, n(k))));
+                        end
+                    end
+                end
+                
+            end % end if there are n & i
+            
+        end % end sample function
     
+    end
     
 end
 
