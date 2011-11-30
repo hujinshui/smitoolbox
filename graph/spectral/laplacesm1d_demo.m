561ab9b9cddb5eacd0653e91a1f6849623dfe7d9
Dahua Lin <lindahua@gmail.com>
2011-11-30 18:57:08 -0500

    graph spectral part modified

--- graph/spectral/laplacesm1d_demo.m
+++ graph/spectral/laplacesm1d_demo.m
@@ -1,12 +1,12 @@
-function laplacesm1d_demo(x0, noise_mag, gker)
+function laplacesm1d_demo(x0, sigma, w)
 % A function to demo 1D Laplacian smoothing
 %
 %   laplacesm1d_demo(x0, noise_mag, mker);
 %
 %   Inputs:
 %       x0:         the sequence of values (not corruped by noise)
-%       noise_mag:  the magnitude of noise
-%       gker:       the grid graph kernel
+%       sigma:      the standard deviation of noise
+%       w:          the neighboring link weights (size 1 x r)
 %
 
 %   History
@@ -26,36 +26,45 @@ if size(x0, 2) > 1
     x0 = x0.';
 end
 
-if ~(isfloat(noise_mag) && isscalar(noise_mag))
+if ~(isfloat(sigma) && isscalar(sigma))
     error('laplacesm1d_demo:invalidarg', ...
-        'noise_mag should be a numeric scalar.');
+        'sigma should be a numeric scalar.');
 end
 
 if nargin < 3
-    gker = 1;
+    w = 1;
+else
+    if ~(isfloat(w) && isreal(w) && ndims(w) == 2 && size(w,1) == 1)
+        error('laplacemat1d_demo:invalidarg', ...
+            'w should be a real row vector.');
+    end
+    w = w.';
 end
+r = numel(w);
+
 
 %% main
 
 % prepare data
 
 n = length(x0);
-x = x0 + randn(size(x0)) * noise_mag;
+x = x0 + randn(size(x0)) * sigma;
 
 % construct MRF
 
-[s,t,w] = gridgraph1d(n, gker);
-g = gr_adjlist.from_edges('u', n, s, t, w);
-a = 1;
+[gb, dx] = gr_local(n, r);  
+ws = repmat(w(abs(dx)), 1, n);
+[Gs, ws] = gr_sym(gb, ws, 'max');
 
 % do smooth
 
-xs = laplacesm(g, a, x);
+a = 1;
+xs = laplacesm(Gs, ws, a, x);
 
 % visualize
 
 figure;
-plot(x, 'b.');
+plot(x, 'b.', 'MarkerSize', 3);
 hold on; plot(x0, 'g-');
 hold on; plot(xs, 'r-');
 
