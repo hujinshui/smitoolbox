85ab44fd8bd533cda2ff194b54d90c54b32a712a
Dahua Lin <lindahua@gmail.com>
2011-12-26 00:06:59 -0500

    start modifying smi_prg framework

---
+++ pmodels/common/smi_state.m
@@ -0,0 +1,21 @@
+classdef smi_state
+    %SMI_STATE the base class of the run-time state of an iterative program
+    %
+    
+    % Created by Dahua Lin, on Dec 26, 2011
+    %
+    
+    methods(Abstract)
+        
+        obj = initialize(obj, varargin);
+        % Initialize the state
+        
+        obj = update(obj);
+        % Update current state
+        
+        R = output(obj);
+        % Make the output from the current state        
+    
+    end
+    
+end
