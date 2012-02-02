5df522b72eb07be3f55d3c533a7fae63b9912933
Dahua Lin <lindahua@gmail.com>
2012-02-02 18:22:54 -0500

    add and test hmm_decode

--- pmodels/markov/chain_viterbi.m
+++ pmodels/markov/chain_viterbi.m
@@ -1,12 +1,13 @@
-function [x, v] = chain_viterbi(A, B)
+function [x, v] = chain_viterbi(A, B, w)
 %CHAIN_VITERBI Viterbi algorithm on a Hidden Markov chain
 %
 %   [x, v] = CHAIN_VITERBI(A, B);
+%   [x, v] = CHAIN_VITERBI(A, B, w);
 %
 %       Performs the Viterbi algorithm along a chain that finds a sequence
 %       of states that maximizes the following objective
 %
-%           sum_{i=1}^n a_i(x_i) + sum_{i=1}^{n-1} b(x_i, x_{i+1})
+%           sum_{i=1}^n a_i(x_i) + sum_{i=1}^{n-1} w(i) * b(x_i, x_{i+1})
 %
 %
 %       Suppose there are K distinct states and n nodes of the chain.
@@ -21,6 +22,9 @@ function [x, v] = chain_viterbi(A, B)
 %                   - K x K matrix, then b(u, v) = B(u, v).
 %                   - 1 x 2 vector, then b(u, v) = B(1) when u == v
 %                     and b(u, v) = B(2) when u != v.
+%
+%       - w:        The second-order potential weights [1 x (n-1)].
+%                   If w is omitted, all weights are assumed to be 1.
 %
 %       Output arguments:
 %       - x:        The resultant sequence of states [1 x n].
@@ -38,6 +42,7 @@ if ~(isfloat(A) && isreal(A) && ~issparse(A) && ndims(A) == 2)
     error('chain_viterbi:invalidarg', ...
         'A should be a non-sparse real matrix.');
 end
+[K, n] = size(A);
 
 if ~( isfloat(B) && isreal(B) && ~issparse(B) && ...
         (isequal(size(B), [K K]) || numel(B) == 2) )
@@ -45,12 +50,23 @@ if ~( isfloat(B) && isreal(B) && ~issparse(B) && ...
         'B should be a non-sparse real matrix of size K x K or 1 x 2.');
 end
 
+if nargin < 3
+    w = [];
+else
+    if ~(isfloat(w) && isreal(w) && ~issparse(w) && isvector(w) && ...
+            length(w) == n - 1)
+        error('chain_viterbi:invalidarg', ...
+            'w should be a non-sparse real vector of length n - 1.');
+    end
+end
+
+
 %% main
 
 if ~isa(A, 'double'); A = double(A); end
 if ~isa(B, 'double'); B = double(B); end
 
-[x, v] = chain_viterbi(A, B);
+[x, v] = chain_viterbi_cimp([], A, B, w);
 
 
 
